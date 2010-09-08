4a4683e46237bb417ee5ba1012e0b62356774a91
unknown <horibo@gmx.de>
2010-09-08 21:27:30 +0100

    Updated gallery config and documentation

--- app/models/gallery_item.rb
+++ app/models/gallery_item.rb
@@ -132,10 +132,10 @@ class GalleryItem < ActiveRecord::Base
    def create_item_infos
 	if self.jpeg? then
 		picture = EXIFR::JPEG.new(self.full_filename)
-		names = %w{model date_time_original exposure_time f_number focal_length iso_speed_ratings}
+		names = Radiant::Config['gallery.exif_names'].split(" ")
 		names.each do |name|
 			value = eval("picture.exif." + name)
-			# f_numbers are stored as rationals (f 4.5 equals to in camera f_number value '9/2')
+			# f_numbers are stored as rationals (f 4.5 equals to f_number value '9/2')
 			# if doing just to_f, the value will be stored as integer in database (4.5 -> 4)
 			value = value.to_f.to_s if name == "f_number"
 			self.infos.create(:gallery_item_id => self.id, :name => name, :value => value)
