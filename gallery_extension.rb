2a4ad36bd8324968414a20f3e83e3ebde220ae50
mikz <m@o2h.cz>
2011-05-06 17:31:47 +0800

    prevent Stack too deep error

--- gallery_extension.rb
+++ gallery_extension.rb
@@ -1,6 +1,7 @@
 require 'tempfile'           
+require 'open-uri'
+
 require_dependency Radiant::Version.to_s.to_f < 0.8 ? 'application' : 'application_controller'   
-require_dependency 'open-uri'
 require_dependency 'exifr/jpeg'
 require_dependency 'exifr/tiff'
 
