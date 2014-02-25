8cf7695356da74b0eec7f24dd2448916c4e3c4c0
Mike Causer <mcauser@gmail.com>
2014-02-25 18:34:28 +1100

    Add tinymce and toggle wysiwyg button to description field in edit item popup

--- public/javascripts/extensions/gallery/gallery/edit_form.js
+++ public/javascripts/extensions/gallery/gallery/edit_form.js
@@ -1,32 +1,50 @@
 var Gallery = Gallery || {};
 
+// prepare a cut down version of the rich text editor for the popups
+tinyMCE.init({
+	editor_selector: 'mceEditor',
+	editor_deselector: 'noMceEditor',
+	mode: 'textareas',
+	theme: 'advanced',
+	theme_advanced_buttons1: 'bold,italic,underline,strikethrough,|,bullist,numlist,charmap,|,undo,redo,|,link,unlink,cleanup,help,code,',
+	theme_advanced_buttons2: '',
+	theme_advanced_buttons3: '',
+	theme_advanced_toolbar_location: 'top',
+	theme_advanced_toolbar_align: 'left',
+	relative_urls: false
+});
+
 Gallery.EditForm = {
 
-    init: function(popup, link) {    
-		
-        if (!this.submitHandler) { this.initializeHandler(); }
-        var item = link.up('.item');
-        popup.down('form').setAttribute('action', link.getAttribute('href'));
+	init: function(popup, link) {
 
-        var name = item.down('.label a').innerHTML;
-        $('edit-item-name').value = (name === '&nbsp;') ? '': name;
-        $('edit-item-credits').value = item.down('.credits').innerHTML;
-        $('edit-item-description').value = item.down('.description').innerHTML;
-        $('edit-item-url').value = item.down('.url').innerHTML;
-        $('edit-item-keywords').value = item.down('.keywords').innerHTML;
-    },                                                                         
+		if (!this.submitHandler) {
+			this.initializeHandler();
+		}
 
-    initializeHandler: function() {
-        this.submitHandler = $('edit-gallery-item-popup').down('form').observe('submit', this.handleFormSubmit);
-    },
+		var item = link.up('.item');
+		popup.down('form').setAttribute('action', link.getAttribute('href'));
 
-    open: function(link) {
-        var popup = $('edit-gallery-item-popup');
-        this.init(popup, link);
-        this.show(popup);
-    },
+		var name = item.down('.label a').innerHTML;
+		$('edit-item-name').value = (name === '&nbsp;') ? '': name;
+		$('edit-item-credits').value = item.down('.credits').innerHTML;
+		$('edit-item-description').value = item.down('.description').innerHTML;
+		$('edit-item-url').value = item.down('.url').innerHTML;
+		$('edit-item-keywords').value = item.down('.keywords').innerHTML;
+	},
 
-    show: function(popup) {  
+	initializeHandler: function() {
+		this.submitHandler = $('edit-gallery-item-popup').down('form').observe('submit', this.handleFormSubmit);
+	},
+
+	open: function(link) {
+		var popup = $('edit-gallery-item-popup');
+		this.init(popup, link);
+		this.show(popup);
+		this.showWysiwyg();
+	},
+
+	show: function(popup) {
 
 		popup.show(); 
 		if( popup.centerInViewport ){
@@ -37,40 +55,53 @@ Gallery.EditForm = {
 		popup.setStyle({
 			top: '100px'
 		}); 
-    },
+	},
 
-    close: function() {
-        var popup = $('edit-gallery-item-popup');
-        popup.hide();
-        this.reset(popup);
-    },
+	close: function() {
+		var popup = $('edit-gallery-item-popup');
+		this.hideWysiwyg();
+		popup.hide();
+		this.reset(popup);
+	},
 
-    reset: function(popup) {
-        popup.down('form').setAttribute('action', '');
-        $('edit-item-name').value = '';
-        $('edit-item-credits').value = '';
-        $('edit-item-description').value = '';
-        $('edit-item-keywords').value = '';
-        $('edit-item-url').value = ''; 
-        $('edit-spinner').hide();
-        $('edit-submit').show();
-    },
+	reset: function(popup) {
+		popup.down('form').setAttribute('action', '');
+		$('edit-item-name').value = '';
+		$('edit-item-credits').value = '';
+		$('edit-item-description').value = '';
+		$('edit-item-keywords').value = '';
+		$('edit-item-url').value = ''; 
+		$('edit-spinner').hide();
+		$('edit-submit').show();
+	},
 
-    handleFormSubmit: function(event) {
-        event.stop();
-        $('edit-spinner').show();
-        $('edit-submit').hide();
-        new Ajax.Request( event.target.getAttribute('action'), {
-            method: 'put',
-            parameters: {
-                'gallery_item[name]': $('edit-item-name').value,
-                'gallery_item[credits]': $('edit-item-credits').value,
-                'gallery_item[description]': $('edit-item-description').value,
-                'gallery_item[keywords]': $('edit-item-keywords').value,    
-                'gallery_item[url]': $('edit-item-url').value,    
-                authenticity_token: $('authenticity_token').value
-            }
-        });
+	toggleWysiwyg: function(){
+		tinyMCE.execCommand('mceToggleEditor', false, 'edit-item-description');
+	},
 
-    }
+	showWysiwyg: function(){
+		tinyMCE.execCommand('mceAddControl', false, 'edit-item-description');
+	},
+
+	hideWysiwyg: function(){
+		tinyMCE.execCommand('mceRemoveControl', false, 'edit-item-description');
+	},
+
+	handleFormSubmit: function(event) {
+		event.stop();
+		this.hideWysiwyg();
+		$('edit-spinner').show();
+		$('edit-submit').hide();
+		new Ajax.Request( event.target.getAttribute('action'), {
+			method: 'put',
+			parameters: {
+				'gallery_item[name]': $('edit-item-name').value,
+				'gallery_item[credits]': $('edit-item-credits').value,
+				'gallery_item[description]': $('edit-item-description').value,
+				'gallery_item[keywords]': $('edit-item-keywords').value,    
+				'gallery_item[url]': $('edit-item-url').value,    
+				authenticity_token: $('authenticity_token').value
+			}
+		});
+	}
 };
